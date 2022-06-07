955f9d74fbcc6b55f0c74c0597062a7ba61cc242
Dmitrij Menkowskij <madaraisback@gmail.com>
2022-06-07 17:55:53 +0200

    add: complete additional task

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,16 @@ const popupWin = document.querySelector('.window-background');
 const closeBtn = popupWin.querySelector('.close-btn');
 const resetBtn = popupWin.querySelector('.reset-counter');
 let textTimes = popupWin.querySelector('.click-times');
-
-const initCount = sessionStorage.getItem("myCount");
+const initCount = sessionStorage.getItem('myCount');
+const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+const table = document.querySelector('.additional-task table');
+const loader = document.querySelector('.loader');
 // open popup
 popupBtn.addEventListener('click', () => {
-    popupWin.style.display = 'flex'
+    popupWin.style.display = 'flex';
+    // counter
     function clickCounter() {
-        if (typeof (Storage) !== "undefined") {
+        if (typeof (Storage) !== 'undefined') {
             if (localStorage.clickCount) {
                 localStorage.clickCount = Number(localStorage.clickCount) + 1;
             } else {
@@ -28,10 +31,40 @@ popupBtn.addEventListener('click', () => {
             this.style.display = 'none';
         });
     }
-});
 
-// close popup
-popupWin.addEventListener("click", function (e) {
-    this.querySelector(".window-body").contains(e.target) || (this.style.display = "none");
+    // additional task
+    getData();
+    async function getData() {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        !response ? showLoader() : hideLoader();
+        showData(data);
+    }
+    function showLoader() {
+        loader.style.display = 'flex';
+    }
+    function hideLoader() {
+        loader.style.display = 'none';
+    }
+    function showData(data) {
+        data.forEach((item) => {
+            const { name, email, address, phone, company } = item;
+            const dataEl = document.createElement("tr");
+
+            dataEl.innerHTML = `
+            <td>${name}</td>
+            <td>${email}</td>
+            <td>${address.city}, ${address.street}, ${address.suite}</td>
+            <td>${phone}</td>
+            <td>${company.name}</td>
+        `;
+
+            table.appendChild(dataEl);
+        });
+    }
 });
-closeBtn.addEventListener("click", () => popupWin.style.display = "none");
+// close popup
+popupWin.addEventListener('click', function (e) {
+    this.querySelector('.window-body').contains(e.target) || (this.style.display = 'none');
+});
+closeBtn.addEventListener('click', () => popupWin.style.display = 'none');
